33ee791e2716d4ac3c0048522948822459860d49
rumesh <rumesh.m.s16@gmail.com>
2020-03-22 18:12:47 +0530

    time intervals , processing data

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import atexit
 import time
+from datetime import datetime
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask import jsonify
 from flask_pymongo import PyMongo
@@ -16,41 +19,86 @@ app.json_encoder = JSONEncoder
 
 @app.route('/', methods=['GET'])
 def get_all():
-    output = mongo.db.applogs.find()
+    output = mongo.db.applogs.find().limit(5)
     result = []
     for i in output:
         result.append(i)
-        print(type(i['time']))
     return jsonify(result)
 
 
-@app.before_first_request
-def before_first():
-    a = int(round(time.time() * 1000))
-    print(a)
-    print(time.time())
+def get_data_in_interval(mongo_resource, start, end):
+    cursor = mongo_resource.find({
+        "time": {
+            "$gte": start,
+            "$lt": end
+        }
+    })
+    # result = []
+    # for i in cursor:
+    #     result.append(i)
+    # print(len(result))
+    return cursor
 
 
-# @app.route('/star/', methods=['GET'])
-# def get_one_star(name):
-#     star = mongo.db.stars
-#     s = star.find_one({'name': name})
-#     if s:
-#         output = {'name': s['name'], 'distance': s['distance']}
-#     else:
-#         output = "No such name"
-#     return jsonify({'result': output})
+def get_result_table(data):
+    result = {}
+    col_name = 'hostname'
+    for i in data:
+        if i[col_name] not in result:
+            result[i[col_name]] = 1
+        else:
+            result[i[col_name]] += 1
+    return result
+
+
+def generate_time_intervals(start, interval_size, end):
+    """
+   :param  (int) interval_size: interval size to break to in milli seconds
+   :param (int) start: start time in milliseconds
+   :param (int) end: end time in milliseconds
+   :return: list of time intervals
+   """
+    start = int(start)
+    end = int(end)
+    interval_list = []
+    n = int((end - start) / interval_size)
+    print("n: " + str(n))
+    counter = start
+    for i in range(n):
+        interval_list.append([counter, counter + interval_size])
+        counter += interval_size
+
+    return interval_list
+
+
+@app.route('/ping/', methods=['GET'])
+def get_ping():
+    mongo_res = mongo.db.applogs
+    interval_size = 20000
+    start = int(datetime.strptime('28.08.1995 00:00:34,00',
+                                  '%d.%m.%Y %H:%M:%S,%f').timestamp() * 10000)
+    end = int(datetime.strptime('28.08.1995 10:01:28,00',
+                                '%d.%m.%Y %H:%M:%S,%f').timestamp() * 10000)
+    interval_list = generate_time_intervals(start, interval_size, end)
+    # print(interval_list)
+    result = []
+    for lst in interval_list:
+        data = get_data_in_interval(mongo_res, lst[0], lst[1])
+        result.append(get_result_table(data))
+    return jsonify(result)
+
+
+# def print_date_time():
+#     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 #
 #
-# @app.route('/star', methods=['POST'])
-# def add_star():
-#     star = mongo.db.stars
-#     name = request.json['name']
-#     distance = request.json['distance']
-#     star_id = star.insert({'name': name, 'distance': distance})
-#     new_star = star.find_one({'_id': star_id})
-#     output = {'name': new_star['name'], 'distance': new_star['distance']}
-#     return jsonify({'result': output})
+# @app.before_first_request
+# def init_scheduler():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
+#     scheduler.start()
+#     # Shut down the scheduler when exiting the app
+#     atexit.register(lambda: scheduler.shutdown())
 
 
 if __name__ == '__main__':
